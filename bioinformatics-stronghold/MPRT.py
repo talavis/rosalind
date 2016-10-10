569a72d7e05f2a8cc7cc5db0a001b72e0bc56af4
Linus Östberg <linus@talavis.eu>
2016-10-10 09:49:05 +0200

    MPRT working

--- bioinformatics-stronghold/MPRT.py
+++ bioinformatics-stronghold/MPRT.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+'''
+Look for the N-glycosylation motif in the sequences of the given proteins
+'''
 
+import sys
 import re
 import requests
-import sys
 
 
 def get_protein(accession):
@@ -11,7 +14,7 @@ def get_protein(accession):
     '''
     try:
         acc = accession[:accesion.index('_')]
-    except:
+    except NameError:
         acc = accession
     base_url = 'http://www.uniprot.org/uniprot/{}.fasta'
     page_req = requests.get(base_url.replace('{}', acc))
@@ -40,6 +43,7 @@ def test_get_protein(capsys):
     out, err = capsys.readouterr()
     assert err == 'Returned data not a FASTA file for {}\n'.format(acc)
 
+
 def find_all_regex(regex, seq):
     '''
     Find all matches for a regex,
@@ -61,8 +65,7 @@ def test_find_all_regex():
     '''
     Test find_all_regex()
     '''
-    NOT_P = 'ACDEFGHIKLMNQRSTVWY'
-    motif = 'N[{}][ST][{}]'.format(NOT_P, NOT_P)
+    motif = 'N(?!P).[ST](?!P).'
     pattern = re.compile(motif)
     acc = 'B5ZC00'
     seq = get_protein(acc)
@@ -71,15 +74,14 @@ def test_find_all_regex():
     seq = get_protein(acc)
     assert find_all_regex(pattern, seq) == [46, 114, 115, 381, 408]
 
-    
+
 def find_motif(sequence):
     '''
     Find all regions containing the N-glycosylation motif
     '''
-    NOT_P = 'ACDEFGHIKLMNQRSTVWY'
-    motif = 'N[{}][ST][{}]'.format(NOT_P, NOT_P)
+    motif = 'N(?!P).[ST](?!P).'
     pattern = re.compile(motif)
-            
+
     return [m + 1 for m in find_all_regex(pattern, sequence)]
 
 
@@ -123,14 +125,14 @@ def test_main(capsys):
     filename = tempfile.mkstemp()[1]
     with open(filename, 'w') as tmpf:
         tmpf.write(accs)
-        
+
     main(filename)
     out, err = capsys.readouterr()
     expected = ('B5ZC00\n' +
                 '85 118 142 306 395\n' +
                 'P07204_TRBM_HUMAN\n' +
                 '47 115 116 382 409\n' +
-                'P20840_SAG1_YEAST\n' + 
+                'P20840_SAG1_YEAST\n' +
                 '79 109 135 248 306 348 364 402 485 501 614\n')
     assert out == expected
 
@@ -140,4 +142,3 @@ if __name__ == '__main__':
         sys.stderr.write('Usage: {} <file with accessions>\n'.format(sys.argv[0]))
         sys.exit(1)
     main(sys.argv[1])
-
